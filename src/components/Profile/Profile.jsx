3ab1d584c089cf370b0faf95439fbf88b3701de5
Alex Polyachinscky <polya4inscky.alex@yandex.ru>
2020-11-13 15:29:24 +0300

    feat: add dialogs page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,9 +3,9 @@ import MyPosts from "./MyPosts";
 import styles from './Profile.module.css'
 const Profile = () => {
   return (
-    <div className="content">
+    <div>
       <div>
-        <img src="https://img5.goodfon.ru/wallpaper/nbig/5/78/zakat-skamia-priroda.jpg" />
+        <img src="https://www.freedigitalphotos.net/images/img/homepage/394230.jpg" />
       </div>
       <div>ava+descr</div>
       <MyPosts/>
