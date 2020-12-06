a58df24eead66aff3fcb3b8645da8fad3eb7440c
Alex Polyachinscky <polya4inscky.alex@yandex.ru>
2020-12-06 16:05:00 +0300

    refactor code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import Header from "./components/Header/Header";
 import Music from "./components/Music";
 import Navbar from "./components/Navbar/Navbar";
 import News from "./components/News";
-import Profile from "./components/Profile/Profile";
 
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
@@ -18,14 +16,8 @@ const App = (props) => {
         <HeaderContainer />
         <Navbar />
         <div className="app-wrapper-content">
-          <Route path='/dialogs' render={()=> <DialogsContainer 
-          // store = {props.store}
-          // state={props.state.messagesPage} dispatch={props.dispatch}
-          />}/>
-          <Route path='/profile/:userId?' render={()=> <ProfileContainer
-          // store = {props.store}
-          // profilePage={props.state.profilePage} dispatch={props.dispatch}
-          />}/>
+          <Route path='/dialogs' render={()=> <DialogsContainer/>}/>
+          <Route path='/profile/:userId?' render={()=> <ProfileContainer/>}/>
           <Route path='/news' component={News}/>
           <Route path='/music' component={Music}/>
           <Route path='/users' component={UsersContainer}/>
